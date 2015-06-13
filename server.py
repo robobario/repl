4a2f85c03feba5c1726a94dc03a2bebb9477c1e4
rob.young <robertyoungnz@gmail.com>
2015-06-13 17:30:39 +1200

    initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from tornado.gen import Task, Return, coroutine
+import tornado.process
+import tornado.web
+from tornado.ioloop import IOLoop
+import re
+from repl import Repl
+repls = {}
+pattern  = re.compile(r"/(\d+)")
+@tornado.web.stream_request_body
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, path):
+        if self.get_argument("close", default=None) is not None:
+          ioloop.stop()
+        self.num = int(path)
+        if self.num not in repls:
+           repls[self.num] = Repl(ioloop, "python")
+        repls[self.num].drain_to_handler(self)     
+   
+    def post(self, path):
+        self.write("")
+
+    def data_received(self, chunk):
+        self.num = int(pattern.match(self.request.path).group(1))
+        repls[self.num].write_async(chunk)
+
+application = tornado.web.Application([
+    (r"/(\d+)", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    ioloop = tornado.ioloop.IOLoop.current()
+    ioloop.start()
