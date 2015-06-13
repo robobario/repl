da6a7e75e80806b20a7a4a8fee07a1487b7e3d03
rob.young <robertyoungnz@gmail.com>
2015-06-13 19:02:20 +1200

    install pip if missing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 sudo apt-add-repository -y ppa:pypy/ppa
 sudo apt-get update
-sudo apt-get -y install git wget pypy
+sudo apt-get -y install git wget pypy python-pip
 pip install virtualenv
 pip install virtualenvwrapper
 mkvirtualenv --no-site-packages --distribute --python=/usr/lib/pypy/bin/pypy-c repl
 pip install tornado
-wget -qO- https://get.docker.com/ | sh
+command -v docker >/dev/null 2>&1 && wget -qO- https://get.docker.com/ | sh
 git clone https://github.com/robobario/repl.git
 cd repl
 sudo python docker.py
