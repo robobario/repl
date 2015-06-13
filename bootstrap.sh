7bd4accd0b8d2e19035cf635d2f3b508c392cc4e
rob.young <robertyoungnz@gmail.com>
2015-06-13 18:58:16 +1200

    add pypy to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
+sudo apt-add-repository -y ppa:pypy/ppa
 sudo apt-get update
-sudo apt-get install git wget
+sudo apt-get -y install git wget pypy
+pip install virtualenv
+pip install virtualenvwrapper
+mkvirtualenv --no-site-packages --distribute --python=/usr/lib/pypy/bin/pypy-c repl
+pip install tornado
 wget -qO- https://get.docker.com/ | sh
 git clone https://github.com/robobario/repl.git
 cd repl
